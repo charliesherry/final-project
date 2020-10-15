d73213550733664d58cb26c3ace967f82f094d87
Carlos Glez <carlosggordon@hotmail.com>
2020-10-15 19:13:14 +0200

    functions file updated

--- src/funciones.py
+++ src/funciones.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import regex as re
+import requests
+from bs4 import BeautifulSoup
+
+
 def decode(lista):
     preps = []
     string = str(lista).replace("\\xa0"," ")
@@ -8,3 +14,29 @@ def decode(lista):
     string5 = string4.replace("\u00bd","1/2")
     preps.append(string5)
     return str(preps[0])
+
+def preptime(a):
+    """This function obtains the preparation time of each recipe
+    Input = URL of the recipe
+    Output= Time of preparation, e.g = "10 mins"
+    """
+    res = requests.get(f'{a}')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    tag = soup.find_all("time")
+    tag = str(tag)
+    a  = re.findall(r"\d\d\ \w+",tag)
+    try:
+        return (a[0])
+    except: 
+        return "Unknown"
+
+def extractimages(a):
+    """This function obtains the main image of each recipe
+    Input = URL of the recipe
+    Output= Image of the recipe
+    """
+    resp = requests.get(f"{a}")
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    tag = soup.find_all("img")[2]
+    b = tag["src"]
+    return b
